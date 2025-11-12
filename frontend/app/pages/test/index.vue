f73be5b31fc362ad637641c7dd73d957c713d87f
fuadiproject <fuadiproject@gmail.com>
2025-11-12 18:26:17 +0700

    feat(frontend): test pdf

---
+++ frontend/app/pages/test/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-full w-full">
+    <iframe
+      class="h-[calc(100vh-340px)] w-full flex-grow object-contain"
+      src="/files/pdf/sample.pdf"
+      type="application/pdf"
+    />
+  </div>
+</template>
