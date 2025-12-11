56abdabd20aff8e235df631303b5f417897a22ac
fuadiproject <fuadiproject@gmail.com>
2025-12-11 23:14:00 +0700

    feat(frontend): remove console

--- frontend/app/components/global/PdfViewerLicenseComponent.vue
+++ frontend/app/components/global/PdfViewerLicenseComponent.vue
@@ -2,7 +2,6 @@
 import { useLicense } from "@vue-pdf-viewer/viewer";
 const config = useRuntimeConfig();
 onBeforeMount(() => {
-  console.log("config.public.vpvLicenseKey : ", config.public.vpvLicenseKey);
   if (config.public.vpvLicenseKey) {
     useLicense(config.public.vpvLicenseKey);
   }
